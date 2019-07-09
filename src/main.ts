23cfe5f826ff936e65d0d5f1b182c62635ac663e
Mateusz Chwedczuk <mateusz.chwedczuk@geekforce.io>
2019-07-09 23:49:25 +0200

    ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ dotenv.config(); /** it has to be called before nests imports */
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 declare const module: any;
 
@@ -20,6 +21,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 4000);
 
   if (module.hot) {
