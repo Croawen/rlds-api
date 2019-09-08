480f5ebbf8cfec2c752b53b051d39649c09044e3
Mateusz Chwedczuk <mateusz.chwedczuk@geekforce.io>
2019-09-08 19:56:32 +0200

    Fixedd incorrect type in get-categories.dto

--- src/transaction/dto/get-categories.dto.ts
+++ src/transaction/dto/get-categories.dto.ts
@@ -2,8 +2,8 @@ import { ApiResponseModelProperty } from '@nestjs/swagger';
 import { TransactionCategory } from '../enums/transaction-category.enum';
 
 export class GetCategoriesDto {
-  @ApiResponseModelProperty()
-  categories: string[];
+  @ApiResponseModelProperty({ type: String })
+  categories: TransactionCategory[];
 
   constructor() {
     this.categories = Object.values(TransactionCategory);
