779ed0a25a139b2b65cff77d00defa204b520499
Mateusz Chwedczuk <mateusz.chwedczuk@geekforce.io>
2019-09-10 19:02:43 +0200

    Added page count field

--- src/common/pager/pager-res.dto.ts
+++ src/common/pager/pager-res.dto.ts
@@ -7,10 +7,13 @@ export class PagerResponseDto {
   pageSize: number;
   @ApiResponseModelProperty()
   totalItems: number;
+  @ApiResponseModelProperty()
+  pageCount: number;
 
   constructor(pageNumber: number, pageSize: number, totalItems: number) {
     this.pageNumber = pageNumber;
     this.pageSize = pageSize;
     this.totalItems = totalItems;
+    this.pageCount = Math.max(Math.round(totalItems / pageSize), 1);
   }
 }
